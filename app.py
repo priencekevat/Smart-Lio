51b89743b4698c8193dd4bb86ae163a46e4e795a
priencekevat <priencekevat6666@gmail.com>
2025-10-04 20:10:30 +0530

    Final push fix for Render

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="static")
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Smart Lio 🚀 Backend is running!"
-
-@app.route('/map')
-def map_page():
-    return render_template('map.html')
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
